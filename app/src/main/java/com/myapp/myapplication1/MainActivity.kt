3fd7eec68054ec5fc12bc4bb22bc4188fe750d40
twinkle-vara <24034211094@gnu.ac.in>
2025-04-03 09:26:01 +0530

    Initial commit

---
+++ app/src/main/java/com/myapp/myapplication1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.myapp.myapplication1
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        val listView = findViewById<ListView>(R.id.lv1)
+        val items = arrayOf("Apple", "Banana", "Orange", "Grapes")
+        val adp=ArrayAdapter(this,android.R.layout.simple_list_item_1,items)
+        listView.adapter=adp
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
